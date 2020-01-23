16c7fadd488d38112faa0e1c9eb54f2cc4ee213f
Аскар <@DESKTOP-Q5Q26T1>
2020-01-23 20:08:37 +0600

    fix create traning-add possible correct time traning

--- Sportclub/Controllers/GraphTraningsController.cs
+++ Sportclub/Controllers/GraphTraningsController.cs
@@ -1,12 +1,13 @@
 ﻿using Microsoft.Ajax.Utilities;
 using Sportclub.Entities;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web.Mvc;
-using WebGrease.Css.Extensions;
+using Newtonsoft.Json;
 
 namespace Sportclub.Controllers
 {
@@ -20,7 +21,33 @@ namespace Sportclub.Controllers
             return View(graphTranings.ToList());
         }
 
-        //------------------------------Create -------------------------------------------       
+        //------------------------------------------Create ------------------------------------------------------      
+        public ActionResult GetTime(int? intDay)    //отправ. самое ранее возм. время
+        {
+            if (intDay == null) 
+                return HttpNotFound();
+            
+            var graphics = db.GraphTranings.Where(g=>(int)g.DayOfWeek == intDay);
+            //занятое время
+            DateTime today = DateTime.Today;
+            var times = graphics.Select(g => new { TimeBegin = g.TimeBegin, TimeEnd = g.TimeEnd }).ToList();
+            times.Add(new { TimeBegin = new DateTime(today.Year, today.Month, today.Day, 21, 0, 0)
+                , TimeEnd= new DateTime(today.Year, today.Month, today.Day, 8, 30, 0) });
+            //times.ForEach(t => System.Diagnostics.Debug.WriteLine("timeBegin: " + t.TimeBegin.GetType() + "; "+ "timeEnd: "+t.TimeEnd.GetType()));
+
+            List<Tuple<DateTime, DateTime>> freeTimes = new List<Tuple<DateTime, DateTime>>();
+            for (int i = 0; i < times.Count()-1; i++) {
+                if (times[i + 1].TimeBegin.Hour - times[i].TimeEnd.Hour >= 2) {
+                    freeTimes.Add(new Tuple<DateTime, DateTime>(
+                        new DateTime (today.Year, today.Month, today.Day,  times[i].TimeEnd.Hour+1, 0, 0)
+                        , new DateTime (today.Year, today.Month, today.Day, times[i].TimeEnd.Hour+2, 30, 0)));
+                }
+            }
+            
+            var json = JsonConvert.SerializeObject(freeTimes);
+            return new JsonResult { Data = json, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+        }
+        [HttpGet]
         public ActionResult Create()    //надо добавить авто-коррекцию врмени в полях ввода в соотв. с граф.
         {
             var coachesId = new SelectList(db.Coaches.Include(nameof(User)), "Id", "User.FullName");
